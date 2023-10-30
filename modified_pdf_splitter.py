51410687510655d3dfdab8b6522950cc69a3e002
Carlos Bonin <carlosbonin@gmail.com>
2023-10-30 16:16:40 -0300

    Update modified_pdf_splitter.py

--- modified_pdf_splitter.py
+++ modified_pdf_splitter.py
@@ -2,14 +2,14 @@ from PyPDF4 import PdfFileReader, PdfFileWriter
 import os
 
 # Original file path
-original_file_path = r'C:/Users/Carlos/Meu Drive/Aulas/Aulas - UEPG/EaD/2024/Introducao a Pratica Extensionista/EDITAL_433_23___Abertura____Professores_Formadores___Curso_de_Licenciatura_em_Matematia____Disciplinas_1__semestre_2024.pdf'
+original_file_path = r'path/file.pdf'
 
 # Read the PDF
 pdf = PdfFileReader(original_file_path)
 num_page = pdf.numPages
 print ("O pdf possui ", num_page , " páginas.")
 
-page = 18
+page = 2 #change this to whatever page you want to split the file in
 
 # Process each page (in this example, just the first page)
 for k in range(page - 1, page):  # Replace range(0, 1) with range(0, num_page) to process all pages
